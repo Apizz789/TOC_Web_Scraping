8441608a4fa4ce4ecd19d08b94b3203b56f090b6
Chatkul Rattanarithikul <billchartkul@hotmail.com>
2022-04-11 15:58:22 +0700

    addCard

--- web_scraping/src/App.js
+++ web_scraping/src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import Searchbox from './components/Searchbox';
+import Card from './components/Card';
 
 function App() {
   return (
     <div className="App">
         <Searchbox/>
+        <Card img={"https://upload.wikimedia.org/wikipedia/commons/thumb/b/b1/VAN_CAT.png/1024px-VAN_CAT.png"} name={"hello my name is brabrabrabrabra"}></Card>
     </div>
   );
 }
