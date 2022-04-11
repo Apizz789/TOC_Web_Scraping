f7a74a337894f0a129175cbb6d130410df9aeb87
mrzombit <54615665+mrzombit@users.noreply.github.com>
2022-04-11 15:57:08 +0700

    fix

--- web_scraping/src/components/Searchbox.js
+++ web_scraping/src/components/Searchbox.js
@@ -1,17 +1,11 @@
 import React from 'react'
+import { useState } from 'react/cjs/react.production.min';
 import '../css/searchbox.style.css'
 
 export default function Searchbox() {
     return (
-        <div>
-
-            <div class="form col-xs-12">
-                <input class="col-xs-9" id="searchBar" type="text" placeholder="search" />
-                <span class="glyphicon glyphicon-search col-xs-1" data-toggle="tooltip" title="Search"></span>
-                <span class="glyphicon bar col-xs-1" id="submit"><b>|</b></span>
-                <a href="https://en.wikipedia.org/wiki/Special:Random" target="_blank"><span class="glyphicon glyphicon-random col-xs-1" data-toggle="tooltip" title="Random topic"></span></a>
-            </div>
-
-        </div>
+        <div></div>
     )
 }
+
+    
