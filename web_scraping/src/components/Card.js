66c1337192bd48ca78c1eccece56095ecbea3558
Chatkul Rattanarithikul <billchartkul@hotmail.com>
2022-04-11 16:42:12 +0700

    fix button

--- web_scraping/src/components/Card.js
+++ web_scraping/src/components/Card.js
@@ -35,7 +35,7 @@ function Card(props) {
                 width:"34px",
                 height:"34px"
             }}
-            onClick={(e) => props.Clickfunction}>+</button>
+            onClick={(e) => props.Clickfunction()}>+</button>
         </div>
         
     </div>)
