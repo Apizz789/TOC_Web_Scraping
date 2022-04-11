981aae71221d1204cadcb9906f4ca08d1b091be3
mrzombit <54615665+mrzombit@users.noreply.github.com>
2022-04-11 22:34:50 +0700

    Update Navbar
    dynamic navbar header

--- web_scraping/src/App.js
+++ web_scraping/src/App.js
@@ -3,12 +3,18 @@ import './App.css';
 import Searchbox from './components/Searchbox';
 import Navbar from './components/Navbar';
 import Content from './components/Content';
+import React, { useState, useEffect } from 'react'
 
 function App() {
+  const [navbarHeader,setNavbarHeader] = useState("category")
+
+  const changePage =(name)=>{
+    setNavbarHeader(name)
+  }
   
   return (
     <div className="App">
-        <Navbar/>
+        <Navbar header ={navbarHeader}/>
         <Searchbox/>
         <Content/>
     </div>
--- web_scraping/src/components/Navbar.js
+++ web_scraping/src/components/Navbar.js
@@ -2,19 +2,17 @@ import React from 'react'
 import '../css/navbar.style.css'
 import { faCartShopping } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import logo from '../resources/icon.png'
 
-
-
-
-export default function Navbar() {
+export default function Navbar(props) {
   return (
     <div class='Navbar'>
       <div class="hor-center">
         <div class="category-toggle">
-          <div>category</div>
+          <div>{props.header}</div>
           <div class="toggle-icon">V</div>
           </div>
-        <div class="header">D</div>
+        <img src={logo} class="logo"/>
         <div class="header">azala</div>
         <FontAwesomeIcon icon={faCartShopping} class="cartIcon"/>
       </div>
