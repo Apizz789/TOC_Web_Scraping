bb8d801a4dcab53e0b734ee1855728c02b176dfd
mrzombit <54615665+mrzombit@users.noreply.github.com>
2022-04-11 16:24:32 +0700

    ddd

--- web_scraping/src/App.js
+++ web_scraping/src/App.js
@@ -1,6 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
-// import Searchbox from './components/Searchbox';
+import Searchbox from './components/Searchbox';
 import Card from './components/Card';
 
 function App() {
@@ -9,9 +9,9 @@ function App() {
   }
   return (
     <div className="App">
-        {/* <Searchbox/> */}
+        <Searchbox/>
         <Card img={"https://upload.wikimedia.org/wikipedia/commons/thumb/b/b1/VAN_CAT.png/1024px-VAN_CAT.png"} name={"hello my name is brabrabrabrabra"}
-        Clickfunction={shoot}></Card>
+        Clickfunction={shoot()}></Card>
     </div>
   );
 }
--- web_scraping/src/components/Searchbox.js
+++ web_scraping/src/components/Searchbox.js
@@ -1,10 +1,9 @@
 import React from 'react'
-import { useState } from 'react/cjs/react.production.min';
 import '../css/searchbox.style.css'
 
 export default function Searchbox() {
     return (
-        <div></div>
+        <div>dfdff</div>
     )
 }
 
