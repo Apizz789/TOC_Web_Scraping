3722101986252cee1c78696ac1fe50f5ff0315e4
Apirak Oulis <82326608+Apizz789@users.noreply.github.com>
2022-04-23 14:42:44 +0700

    .

--- web_scraping/src/resources/user.dummy.js
+++ web_scraping/src/resources/user.dummy.js
@@ -8,9 +8,27 @@ const users = [
         cart: null
     },
     {
+        username: "admin0",
+        password: "12345",
+        name: "คุณมาโนช",
+        cart: cart[0]
+    },
+    {
         username: "admin1",
         password: "12345",
         name: "คุณเค",
+        cart: cart[0]
+    },
+    {
+        username: "admin2",
+        password: "12345",
+        name: "คุณมานะ",
+        cart: cart[0]
+    },
+    {
+        username: "admin3",
+        password: "12345",
+        name: "คุณมานี",
         cart: cart[0]
     },
     {
