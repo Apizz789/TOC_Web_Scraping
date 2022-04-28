9258a10a7885b67c100b70d10ef5b4600c2d3954
Chatkul Rattanarithikul <billchartkul@hotmail.com>
2022-04-28 21:24:47 +0700

    UpdateDemo

--- web_scraping/src/components/Contents/Cart/Demo.js
+++ web_scraping/src/components/Contents/Cart/Demo.js
@@ -1,9 +1,72 @@
-import React from 'react'
+import React, { useState } from 'react'
 import '../../../css/component.style.css'
+import "../../../css/demo.style.css"
+import ListProduct from "../../../resources/compare.dummy"
 
 function Demo(props) {
+  const [Product,setProduct] = useState(
+    {
+      lazada: {
+          name: '',
+          price: "",
+      },
+      Welove: {
+          name: '',
+          price: "",
+      },
+      ebay: {
+          name: '',
+          price: "",
+      }
+  }
+  );
+  function optionChange(event){
+    setProduct(ListProduct[event.target.value]);
+  } 
   return (
-    <div>Demo : {props.header}
+    <div>
+      <div style={{ display: "flex", flexDirection: "column", width:"100%", alignItems:"center", marginTop:"2%"}}>
+        <div style={{display:"flex",width:"100%",justifyContent:"center"}}>
+          <select style={{width:"65%",height:"60px",paddingLeft:"1%",fontSize:"28px"}} onChange={optionChange}>
+            <option value="" selected disabled hidden>เลือกรุ่นโทรศัพท์/ไอแพด/ฯลฯ</option>
+            <option value="0" class='optionDemo'>Apple iPhone 12</option>
+            <option value="1" class='optionDemo'>Samsung Galaxy S10 Lite (RAM8GB/ROM128GB)</option>
+            <option value="2" class='optionDemo'>Samsung Galaxy A32 5G 128GB</option>
+            <option value="3" class='optionDemo'>Apple iPhone 11 128GB</option>
+            <option value="4" class='optionDemo'>Apple iPhone SE 128 GB</option>
+            <option value="5" class='optionDemo'>IPHONE 11 64GB</option>
+            <option value="6" class='optionDemo'>Apple iPad Air 2 Wi-Fi + Cellular 16GB (Space Gray)</option>
+            <option value="7" class='optionDemo'>Huawei MediaPad M5 Pro</option>
+            <option value="8" class='optionDemo'>APPLE Apple Pencil (2nd Generation)</option>
+          </select>
+        </div>
+        <div style={{ display: "flex", justifyContent:"Space-around", width:"100%",marginTop:"2%"}}>
+          <div style={{ display: "flex", flexDirection: "column",alignItems:"center",width:"20%",}}>
+            <div class="DemoTopic"><h2>Lazada</h2></div>
+            <div class="DemoContentBox">
+              <h3>{Product.lazada.name}</h3>
+              <hr style={{width:"75%",textAlign:"left",marginLeft:"0"}}/>
+              <h1 class="DemoCost">{Product.lazada.price}</h1>
+            </div>
+          </div>
+          <div style={{ display: "flex", flexDirection: "column",alignItems:"center",width:"20%",}}>
+            <div class="DemoTopic"><h2>We love shopping</h2></div>
+            <div class="DemoContentBox">
+              <h3>{Product.Welove.name}</h3>
+              <hr style={{width:"75%",textAlign:"left",marginLeft:"0"}}/>
+              <h1 class="DemoCost">{Product.Welove.price}</h1>
+            </div>
+          </div>
+          <div style={{ display: "flex", flexDirection: "column",alignItems:"center",width:"20%",}}>
+            <div class="DemoTopic"><h2>Ebay</h2></div>
+            <div class="DemoContentBox">
+              <h3>{Product.ebay.name}</h3>
+              <hr style={{width:"75%",textAlign:"left",marginLeft:"0"}}/>
+              <h1 class="DemoCost">{Product.ebay.price}</h1>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
 
   )
---
+++ web_scraping/src/resources/compare.dummy.js
@@ -0,0 +1,129 @@
+const products = [
+    {
+        lazada: {
+            name: 'Apple iPhone 12',
+            price: "THB22,990.00",
+        },
+        Welove: {
+            name: 'Apple iPhone 12 - แอปเปิ้ล 64 GB',
+            price: "THB36,500",
+        },
+        ebay: {
+            name: 'Apple iPhone 12 64GB Black Blue Green Red White T-Mobile Very Good Condition',
+            price: "THB15,818.89",
+        }
+    },
+    {
+        lazada: {
+            name: 'Samsung Galaxy S10 5G Ram8GB Rom256GB 6.7 inches',
+            price: "THB13,800.00",
+        },
+        Welove: {
+            name: 'Samsung Galaxy S10 Lite (RAM8GB/ROM128GB)',
+            price: "THB19,500",
+        },
+        ebay: {
+            name: 'Samsung Galaxy S10 Black Sprint AT&T T-Mobile Verizon Factory Unlocked VERY GOOD',
+            price: "THB7,531.99",
+        }
+    },
+    {
+        lazada: {
+            name: 'Samsung Galaxy A32 4G (8+128GB) (SM-A325) (By Lazada Superiphone)',
+            price: "THB7,620.00",
+        },
+        Welove: {
+            name: 'Samsung Galaxy A32 5G 128GB',
+            price: "THB13,644",
+        },
+        ebay: {
+            name: 'Samsung Galaxy A32 - 64GB - Factory Unlocked - Very Good Condition',
+            price: "THB6,190.33",
+        }
+    },
+    {
+        lazada: {
+            name: 'Apple iPhone 11 [iStudio by UFicon]',
+            price: "THB19,500.00",
+        },
+        Welove: {
+            name: 'Apple iPhone 11 128GB',
+            price: "THB25,290",
+        },
+        ebay: {
+            name: 'Apple iPhone 11 Pro Max A2161 ATT T-Mobile Verizon Factory Unlocked - VERY GOOD',
+            price: "THB21,289.04",
+        }
+    },
+    {
+        lazada: {
+            name: 'iPhone SE (2022)',
+            price: "THB14,999.00",
+        },
+        Welove: {
+            name: 'Apple iPhone SE 128 GB',
+            price: "THB19,500",
+        },
+        ebay: {
+            name: 'Apple iPhone SE 2nd Gen 64GB Unlocked AT&T T-Mobile Verizon Very Good Condition',
+            price: "THB7,220.73",
+        }
+    },
+    {
+        lazada: {
+            name: 'iPhone 11 without Adapter',
+            price: "THB17,200.00",
+        },
+        Welove: {
+            name: 'IPHONE 11 64GB',
+            price: "THB26,900",
+        },
+        ebay: {
+            name: 'Apple iPhone 11 - 64GB - Factory Unlocked - Excellent Condition',
+            price: "THB13,068.85",
+        }
+    },
+    {
+        lazada: {
+            name: '10.9-inch iPad Air Wi-Fi (THA)',
+            price: "THB17,300.00",
+        },
+        Welove: {
+            name: 'Apple iPad Air 2 Wi-Fi + Cellular 16GB (Space Gray)',
+            price: "THB17,800",
+        },
+        ebay: {
+            name: 'Apple MYFM2LL/A iPad Air 4th Gen 10.9-inch 64GB Wi-Fi - New / Free Fast Shipping',
+            price: "THB15,167.34",
+        }
+    },
+    {
+        lazada: {
+            name: 'HUAWEI MediaPad M5 lite 8″ RAM 3 GB ROM 32 GB ประกันศูนย์ไทย 1 ปีเต็ม โทรออกได้ ICOM',
+            price: "THB7,390.00",
+        },
+        Welove: {
+            name: 'Huawei MediaPad M5 Pro',
+            price: "THB17,990",
+        },
+        ebay: {
+            name: 'Huawei MediaPad M5 Lite BAH2-W19 32 GB, Wi-Fi, 10.1 in - Space Grey',
+            price: "THB1,806.26",
+        }
+    },
+    {
+        lazada: {
+            name: 'Apple Pencil (2nd Generation)',
+            price: "THB4,090.00",
+        },
+        Welove: {
+            name: 'Apple Pencil 2nd Generation',
+            price: "THB2,838.41",
+        },
+        ebay: {
+            name: 'Apple iPhone 12 64GB Black Blue Green Red White T-Mobile Very Good Condition',
+            price: "THB15,818.89",
+        }
+    },
+]
+export default products;
