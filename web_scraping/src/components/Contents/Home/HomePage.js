28477754f20ae2cfe0fd5b6f4946e0476c31a977
Tum Chatdanai <54245406+Brackhak@users.noreply.github.com>
2022-04-28 16:56:19 +0700

    Add files via upload

--- web_scraping/src/components/Contents/Home/HomePage.js
+++ web_scraping/src/components/Contents/Home/HomePage.js
@@ -5,27 +5,27 @@ import Banner from '../Home/Banner'
 import { useState, useEffect } from "react";
 import axios from 'axios'
 const data_trend = [
-  {
-  name: '7" - 24" Digital TV 12v 240v for Motorhome Caravan Boat DVB-T2 Freeview PVR',
-  price: "THB17,036.37",
-  img: ["https://i.ebayimg.com/images/g/V0oAAOSwapdbveOc/s-l300.jpg"],
-},
-{
-  name: "Hisense 55AE7000F - Smart TV LED Ultra HD 4K, HDR 10+, Dolby DTS, con Alexa",
-  price: "THB17,036.37",
-  img: ["https://i.ebayimg.com/images/g/XbsAAOSw2m1iUwSW/s-l300.jpg"],
+//   {
+//   name: '7" - 24" Digital TV 12v 240v for Motorhome Caravan Boat DVB-T2 Freeview PVR',
+//   price: "THB17,036.37",
+//   img: ["https://i.ebayimg.com/images/g/V0oAAOSwapdbveOc/s-l300.jpg"],
+// },
+// {
+//   name: "Hisense 55AE7000F - Smart TV LED Ultra HD 4K, HDR 10+, Dolby DTS, con Alexa",
+//   price: "THB17,036.37",
+//   img: ["https://i.ebayimg.com/images/g/XbsAAOSw2m1iUwSW/s-l300.jpg"],
   
-},
-{
-  name: "Hisense 55AE7000F - Smart TV LED Ultra HD 4K, HDR 10+, Dolby DTS, con Alexa",
-  price: "THB17,036.37",
-  img: ["https://i.ebayimg.com/images/g/XbsAAOSw2m1iUwSW/s-l300.jpg"],
-},
-{
-  name: "Hisense 55AE7000F - Smart TV LED Ultra HD 4K, HDR 10+, Dolby DTS, con Alexa",
-  price: "THB17,036.37",
-  img: ["https://i.ebayimg.com/images/g/XbsAAOSw2m1iUwSW/s-l300.jpg"],
-},
+// },
+// {
+//   name: "Hisense 55AE7000F - Smart TV LED Ultra HD 4K, HDR 10+, Dolby DTS, con Alexa",
+//   price: "THB17,036.37",
+//   img: ["https://i.ebayimg.com/images/g/XbsAAOSw2m1iUwSW/s-l300.jpg"],
+// },
+// {
+//   name: "Hisense 55AE7000F - Smart TV LED Ultra HD 4K, HDR 10+, Dolby DTS, con Alexa",
+//   price: "THB17,036.37",
+//   img: ["https://i.ebayimg.com/images/g/XbsAAOSw2m1iUwSW/s-l300.jpg"],
+// },
 ]
 
 
@@ -37,24 +37,23 @@ export default function Homepage(props) {
   }
 
 
-  const [data, setData] = useState({})
-  const [url,setUrl] = useState("http://43.228.85.133:80/realtime/")
-  useEffect(() =>{ 
+  const [itemProduct, setData] = useState()
+  const [url, setUrl] = useState("http://43.228.85.133:80/realtime/")
+
+  useEffect(() => {
     axios.get(url).then(res => {
       const data_trend = res.data
-      
+      setData(data_trend.map((e) =>
+        <Card product={e} doFunc={shoot}></Card>
+      ))
+
       console.log(data_trend)
-      console.log("Hi")
-      
+
       // data.map((each)=> alert(each.name))
     }).catch(error => {
       console.log(error.response)
     })
-  },[url])
-  
-  const itemProduct = data_trend.map((e) =>
-        <Card product={e} doFunc={shoot}></Card>
-  );
+  }, [url])
 
 
   return (
