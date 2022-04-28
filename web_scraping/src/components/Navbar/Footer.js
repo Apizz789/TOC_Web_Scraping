27f09f3bb42c28c859ddd76e2373e16547ec287d
Apirak Oulis <82326608+Apizz789@users.noreply.github.com>
2022-04-29 05:10:35 +0700

    upload pic fix

--- web_scraping/src/components/Navbar/Footer.js
+++ web_scraping/src/components/Navbar/Footer.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import '../../css/footer.style.css'
 
-const contact_img = "https://th.qr-code-generator.com/wp-content/themes/qr/new_structure/markets/basic_market/generator/dist/generator/assets/images/websiteQRCode_noFrame.png"
+const contact_img = "https://th.qr-code-generator.com/wp-content/https://raw.githubusercontent.com/Apizz789/TOC_Web_Scraping/main/web_scraping/public/images/QR.png/qr/new_structure/markets/basic_market/generator/dist/generator/assets/images/websiteQRCode_noFrame.png"
 const contact_url = "www.google.com"
 
 function Footer(props) {
@@ -12,17 +12,25 @@ function Footer(props) {
                 <div class="footer-container-child">Dazala © 2022</div>
             </div>
 
-            <div class="footer-container box2" >
+            <div class="footer-container box2">
 
-
-                <div class="footer-container-child" onClick={(e) => props.pageHandle("Home", "ContactUs")} >
-                    <div  >ContactUs</div>
+                <div class="footer-container-child" onClick={(e) => props.pageHandle("ContactUs", "Home")}>
+                    <div >ContactUs</div>
                 </div>
 
             </div>
             <div class="footer-container box1">
                 <div class="footer-container-child"><img class="contact-img" src={contact_img} /></div>
             </div>
+
+
+
+
+
+
+
+
+            
             </div>
 
     )
