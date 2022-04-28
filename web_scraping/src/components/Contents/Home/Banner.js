05babba752b0b671234d8b9007a0a4b914a3cd24
Apirak Oulis <82326608+Apizz789@users.noreply.github.com>
2022-04-29 02:28:54 +0700

    serchbar.. border

--- web_scraping/src/components/Contents/Home/Banner.js
+++ web_scraping/src/components/Contents/Home/Banner.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import './../../../css/banner.style.css'
-
 import Carousel from 'react-bootstrap/Carousel'
 
 function Banner() {
@@ -13,65 +12,51 @@ function Banner() {
                   className="d-block w-100"
                   src="../images/banner1.png"
                   alt="First slide"
-                  style={{backgroundColor:'grey',objectFit:'cover',borderRadius:"20px"}}
+                  style={{height:'50vh', width:'100vw',backgroundColor:'grey',objectFit:'cover',borderRadius:"20px"}}
                 />
-                <Carousel.Caption>
-                {/* <img src="../images/RTS=LOGO2.png" style={{height:'25vh',objectFit:'contain'}} /><br/><br/> <hr/> */}
-                  <h3 >Dazala</h3>
-                  <p >WEB Application for everyone who want to shopping, just enjoy with us.</p>
-                </Carousel.Caption>
+                
               </Carousel.Item>
-
 
               <Carousel.Item>
                 <img
                   className="d-block w-100"
                   src="../images/banner2.png?"
                   alt="First slide"
-                  style={{backgroundColor:'grey',objectFit:'cover',borderRadius:"20px"}}
+                  style={{height:'50vh', width:'100vw',backgroundColor:'grey',objectFit:'cover',borderRadius:"20px"}}
                 />
-                <Carousel.Caption>
-                {/* <img src="../images/BTS-Logo.png" style={{height:'25vh',objectFit:'contain'}} /> <br/><br/> <hr/> */}
-                  <h3 >Lazada</h3>
-                  <p >The international e-commerce company founded by Maximilian Bittner</p>
-                </Carousel.Caption>
+               
               </Carousel.Item>
-
 
               <Carousel.Item>
                 <img
                   className="d-block w-100"
                   src="../images/banner3.png"
                   alt="Second slide"
-                  style={{backgroundColor:'grey',objectFit:'cover',borderRadius:"20px"}}
+                  style={{height:'50vh', width:'100vw',backgroundColor:'grey',objectFit:'cover',borderRadius:"20px"}}
                 />
 
-                <Carousel.Caption>
-                {/* <img src="../images/MRT-Logo.png" style={{height:'25vh',objectFit:'contain'}}/>  <br/><br/> <hr/> */}
-                  <h3 >We Love Shopping</h3>
-                  <p >Shopee Pte. Ltd. (Shopee) is a Singaporean multinational technology company which focuses mainly on e-commerce.</p>
-                </Carousel.Caption>
+                
               </Carousel.Item>
-
 
               <Carousel.Item>
                 <img
                   className="d-block w-100"
                   src="../images/banner4.png"
                   alt="Third slide"
-                  style={{backgroundColor:'grey',objectFit:'cover',borderRadius:"20px"}}
+                  style={{height:'50vh', width:'100vw',backgroundColor:'grey',objectFit:'cover',borderRadius:"20px"}}
                 />
 
-                <Carousel.Caption>
-                {/* <img src="../images/ARL-Logo.png" style={{height:'25vh',objectFit:'contain'}}/> <br/><br/> <hr/> */}
-                  <h3 >EBAY</h3>
-                  <p >eBay Inc. is an American multinational e-commerce corporation based in San Jose, California.</p>
-                </Carousel.Caption>
+                
               </Carousel.Item>
 
             </Carousel>
         </div>
     </div>
+
+
+
+
+
 
   )
 }
