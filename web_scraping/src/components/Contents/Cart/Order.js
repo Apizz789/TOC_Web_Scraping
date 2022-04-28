4887fb927d3e9d86d9b1970f7ce6187df0162d72
Apirak Oulis <82326608+Apizz789@users.noreply.github.com>
2022-04-28 23:45:23 +0700

    del all

--- web_scraping/src/components/Contents/Cart/Order.js
+++ web_scraping/src/components/Contents/Cart/Order.js
@@ -4,45 +4,7 @@ import '../../../css/oder.css'
 function Order(props) {
   return (
 
-    <div className="text-center">
-
-
-      <div className="container">
-
-
-        <div class="col box-cart-main">
-
-          <div class="row ">
-
-
-            <div class="text-cart-header"><center><h1>My Order</h1></center></div>
-
-          </div>
-
-
-          <div className="col box-cart-table">
-            <div class="column"><center><b>HEADER LIST</b></center></div>
-            <div class="header-table">
-              <center>
-                <b></b>
-              </center>
-              <hr></hr>
-            </div>
-
-          </div>
-          <div className="col box-cart-table">
-            <div class="column"><center><b>HEADER LIST</b></center></div>
-            <div class="header-table">
-              <center>
-                <b></b>
-              </center>
-              <hr></hr>
-            </div>
-
-          </div>
-
-
-          <center>
+         <center>
           <div class="component-btn" onClick={(e) => props.pageHandle("Cart","Spinlucky")}>
               <center>
                 <h2>Spinlucky
@@ -50,26 +12,7 @@ function Order(props) {
                 </h2>
               </center>
             </div>
-
-            {/* <div class="component-btn" onClick={(e) => props.pageHandle("Cart","Reciept")}>
-              <center>
-                <h2>Go Reciept</h2>
-              </center>
-            </div> */}
           </center>
-        </div>
-      </div>
-    </div>
-
-
-    // <div>Order
-    //   <div class="component-btn" onClick={(e) => props.pageHandle("Cart","Reciept")}> go Reciept</div>
-    //        </div>
-
-      
-
-    
-    
 
   )
 }
--- web_scraping/src/components/Contents/Cart/Reciept.js
+++ web_scraping/src/components/Contents/Cart/Reciept.js
@@ -5,45 +5,6 @@ function Reciept(props) {
   return (
 
 
-
-    <div className="text-center">
-
-
-    <div className="container">
-
-
-      <div class="col box-cart-main">
-
-        <div class="row ">
-
-
-          <div class="text-cart-header"><center><h1>Reciept</h1></center></div>
-
-        </div>
-
-
-        <div className="col box-cart-table">
-          <div class="column"><center><b>HEADER LIST</b></center></div>
-          <div class="header-table">
-            <center>
-              <b></b>
-            </center>
-            <hr></hr>
-          </div>
-
-        </div>
-        <div className="col box-cart-table">
-          <div class="column"><center><b>HEADER LIST</b></center></div>
-          <div class="header-table">
-            <center>
-              <b></b>
-            </center>
-            <hr></hr>
-          </div>
-
-        </div>
-
-
         <center>
           <div class="component-btn" onClick={(e) => props.pageHandle("Order","Payment")}> 
             <center>
@@ -51,17 +12,7 @@ function Reciept(props) {
             </center>
           </div>
         </center>
-      </div>
-    </div>
-  </div>
-
-    // <div >Reciept
-    //     <div class="component-btn" onClick={(e) => props.pageHandle("Order","Payment")}> go Payment</div>
-    // </div>
-
-
-
-
+   
 
   )
 }
